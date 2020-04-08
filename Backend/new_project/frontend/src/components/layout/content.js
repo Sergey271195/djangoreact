d8787897d2ffe86160193b42f5009a9aa6938625
Sergey <mackovckin.sergei@yandex.ru>
2020-04-08 08:54:08 +0300

    react basics pt.2

--- Backend/new_project/frontend/src/components/layout/content.js
+++ Backend/new_project/frontend/src/components/layout/content.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react'
+import toDoData from './toDoData.js'
+
 
 export class ToDoItem extends Component {
-    render() {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            completed: this.props.completed
+        }
+    }
+
+    render()
+        {
+            if (this.props.label) {
+                this.label_style = {display: 'block'}
+            }
+            else {
+                this.label_style = {display: 'none'}
+            }
     return (
-        <div className = 'input-group border-bottom border-success mt-2 ml-2 p-2 w-75'>
-            <input type = 'checkbox' className = 'form-check-input' />
-                <label className = 'mb-0'>Learn basics of React</label>
-                    <div className = 'container'>
-                        <p><br />In order to learn basics of React one should first learn basics of Javascript<br />
-                        JS is language taht is widely used in web programming.<br />
-                        React is JS framework. One of the most ppopular ones<br />
-                        Others being Node.js, Vue.js and Angular...</p>
+        <div onMouseOver = {function(event) {event.target.className += ' .bg-succsess'}} className = 'input-group border-bottom border-success justify-content-start mt-2 ml-2 p-2 w-75'>
+                <label className = 'mb-0 checkbox-inline' style = {this.label_style}>
+                    <input type = 'checkbox' className = 'form-check-input' checked = {this.state.completed}/>  
+                    {this.props.label} 
+                </label>
+                    <div className = 'container w-100 my-2 '>
+                            {this.props.main}
                     </div>
         </div>
     )}
@@ -29,80 +45,61 @@ export class ToDoList extends Component {
     }
 }
 
-function MyInfo()  {
+class MyInfo extends Component  {
+
+    constructor() {
+        super()
+        this.state = {
+            toDoData: toDoData
+        }
+    }
+
+    
+
+    userInfo() {
         const firstname = 'Sergey'
         const lastname = 'Makovkin'
-        const date = new Date()
-        const hours = date.getHours()
-        let dayTime
-        let styles = {
-            justifyContent: 'center'
-        }
-        if (hours > 12) {
-            dayTime = 'Evening'
-        }
-        else {
-            dayTime = 'Morning'
-        }
+        let user = {
+        imgSrc: 'https://avatars.mds.yandex.net/get-pdb/2836723/a1091df8-4b0f-4b8e-8b7a-7ec9122946e1/s1200?webp=false',
+        firstname: firstname,
+        lastname: lastname}
+        return user
+    } 
+
+    render() {
+
+        const TaskComponents = this.state.toDoData.filter(task => task.label).map(function(task) {
+                return (<ToDoItem key = {task.id} label={task.label} main = {task.main} completed = {task.completed}/>)
+        })
+        const userinfo = this.userInfo()
         return(
         <div className = 'container-fluid p-5'>
             <div className = 'row justify-content-center'>
-            <div className = 'col-5'>
-            <h1 className = 'mt-5'>{`${firstname} ${lastname}`}</h1>
-            <div className = 'container mt-3 mb-3' style = {styles}>
-                {`Good ${dayTime} to You, ${firstname} ${lastname}`}
+            <div className = 'col-3'>
+            <img src = {userinfo.imgSrc} className = 'w-50 rounded-circle' />
+            <h1 className = 'mt-5'>{`${userinfo.firstname} ${userinfo.lastname}`}</h1>
+            <div className = 'container mt-3 mb-5'>
+                Well. What can i say? <br />
+                Trying to learn functional programming is not that simple. Though it is quiet interesting.<br />
+                But i think that i prefer object oriented programming. Thx python. One love
             </div>
-                <div className = 'container w-75'>
-                    <p>Well. What can i say? <br />
-                    Trying to learn functional programming is not that simple. Though it is quiet interesting.<br />
-                        But i think that i prefer object oriented programming. Thx python. One love
-                    </p>
-                </div>
+
+                <button type="button" onClick = {() => {console.log(this.userInfo())}} className="btn btn-success mr-2">Add Task</button>
+                <button type="button" className="btn btn-success">Show Completed</button>
+
             </div>
-            <div className = 'col-5'>
+            <div className = 'col-6'>
+
             <h1 className ='mt-5'>To-do list</h1>
+                <div className = ' d-flex flex-column align-items-center border border-success mt-2 mb-2 py-4 w-100'>
 
-            <button type="button" className="btn btn-success">Add Task</button>
-
-            <div className = ' d-flex flex-column align-items-center border border-success mt-2 mb-2 py-4 w-100'>
-                
-                <div className = 'input-group border-bottom border-success mt-2 ml-2 p-2 w-75'>
-                    <input type = 'checkbox' className = 'form-check-input' />
-                    <label className = 'mb-0'>Learn basics of React</label>
-                    <div className = 'container'>
-                        <p><br />In order to learn basics of React one should first learn basics of Javascript<br />
-                        JS is language taht is widely used in web programming.<br />
-                        React is JS framework. One of the most ppopular ones<br />
-                        Others being Node.js, Vue.js and Angular...</p>
-                    </div>
-                </div>
-
-                <div className = 'input-group border-bottom border-success mt-2 ml-2 p-2 w-75'>
-                    <input type = 'checkbox' className = 'form-check-input'/>
-                        <label className = 'mb-0'>Learn basics of Django</label>
-                        <div className = 'container'>
-                            <p><br />In order to learn basics of Django one should first learn basics of Python<br />
-                            Python is beginners friendly. So don't worry too much.<br />
-                            Anything could be achieved with persistance and labour</p>
-                        </div>
-                </div>
-
-                <div className = 'input-group border-bottom border-success m-2 ml-2 p-2 w-75'>
-                    <input type = 'checkbox' className = 'form-check-input'/>
-                    <label className = 'mb-0'>Combine both</label>
-                    <div className = 'container'>
-                            <p><br />So in order to become so called fullstack programmer you should know a lot...<br />
-                            But it worth a try, I think.<br />
-                            So, let's go for it together to shiny dream of freedom and what not</p>
-                    </div>
-                </div>
-
-                <ToDoItem />
+                    {TaskComponents}
 
                 </div>
             </div>
             </div>
         </div>)
+    }
 }
 
 export default MyInfo
